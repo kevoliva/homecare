b696e7bdfee6cf693a58ac217f79bf3ea9b2a009
Kévin Oliva <olivakevin64@gmail.com>
2020-01-14 14:51:39 +0100

    Création du contrôleur HomecareController

---
+++ src/Controller/HomecareController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomecareController extends AbstractController
+{
+    /**
+     * @Route("/homecare", name="homecare")
+     */
+    public function index()
+    {
+        return $this->render('homecare/index.html.twig', [
+            'controller_name' => 'HomecareController',
+        ]);
+    }
+}
