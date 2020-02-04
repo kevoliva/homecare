2aa8e32c3116da94a70972dd971a3e3cd5dc180a
Kévin Oliva <olivakevin64@gmail.com>
2020-02-04 22:10:40 +0100

    Création du CRUD pour Autorisation

---
+++ src/Form/AutorisationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Autorisation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AutorisationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('plan')
+            ->add('facture')
+            ->add('intervention')
+            ->add('alerte')
+            ->add('professionnel')
+            ->add('bien')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Autorisation::class,
+        ]);
+    }
+}
