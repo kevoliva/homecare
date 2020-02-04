a2eaaf0cf7ddd5a6d66f992595f7e9663b6f40f1
Kévin Oliva <olivakevin64@gmail.com>
2020-02-04 22:02:34 +0100

    Création du CRUD pour Intervention

---
+++ src/Form/InterventionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Intervention;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InterventionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle')
+            ->add('typeInterv')
+            ->add('observation')
+            ->add('remarque')
+            ->add('bien')
+            ->add('alerte')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Intervention::class,
+        ]);
+    }
+}
