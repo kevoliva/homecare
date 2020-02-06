1843d0eee7c3a2eea0dcac1a1dcb65e1b3f642cc
Kévin Oliva <olivakevin64@gmail.com>
2020-02-06 17:21:18 +0100

    Ajout du contrôleur Plan PROFESSIONNEL ainsi que la vue index + routes

---
+++ src/Controller/ProPlanController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Plan;
+use App\Form\PlanType;
+use App\Repository\PlanRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/pro/plan")
+*/
+class ProPlanController extends AbstractController
+{
+  /**
+  * @Route("/", name="pro_plan_index", methods={"GET"})
+  */
+  public function index(PlanRepository $planRepository): Response
+  {
+    return $this->render('professionnel/plan/index.html.twig', [
+      'plans' => $planRepository->findAll(),
+    ]);
+  }
+
+
+
+  /**
+  * @Route("/{id}", name="pro_plan_show", methods={"GET"})
+  */
+  public function show(Plan $plan): Response
+  {
+    return $this->render('plan/show.html.twig', [
+      'plan' => $plan,
+    ]);
+  }
+}
