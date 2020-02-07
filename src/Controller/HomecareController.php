51ba107df5be7e8138870e19a69de19e0b736336
Kévin Oliva <olivakevin64@gmail.com>
2020-02-07 19:51:36 +0100

    Ajout d'une page d'accueil qui redirige vers my(propriétaire) ou vers pro(professionnel)

--- src/Controller/HomecareController.php
+++ src/Controller/HomecareController.php
@@ -7,24 +7,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomecareController extends AbstractController
 {
-    /**
-     * @Route("/my/", name="homecare")
-     */
-    public function index()
-    {
-        return $this->render('homecare/index.html.twig', [
-            'controller_name' => 'HomecareController',
-        ]);
-    }
+
+  /**
+  * @Route("/", name="homecare")
+  */
+  public function index()
+  {
+    return $this->render('accueil.html.twig', [
+      'controller_name' => 'HomecareController',
+    ]);
+  }
+
+  /**
+  * @Route("/my/", name="homecare_my")
+  */
+  public function indexMy()
+  {
+    return $this->render('homecare/index.html.twig', [
+      'controller_name' => 'HomecareController',
+    ]);
+  }
 
 
-    /**
-     * @Route("/pro/", name="homecare_pro")
-     */
-    public function indexPro()
-    {
-        return $this->render('professionnel/homecare/index.html.twig', [
-            'controller_name' => 'HomecareController',
-        ]);
-    }
+  /**
+  * @Route("/pro/", name="homecare_pro")
+  */
+  public function indexPro()
+  {
+    return $this->render('professionnel/homecare/index.html.twig', [
+      'controller_name' => 'HomecareController',
+    ]);
+  }
 }
