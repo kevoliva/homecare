25774427e67865aca6151d81d7cd8ec1e235477b
Kévin Oliva <olivakevin64@gmail.com>
2020-02-16 15:35:15 +0100

    Retrait de la rubrique 'intervention' dans le formulaire d'ajout d'alerte

--- src/Form/AlerteType.php
+++ src/Form/AlerteType.php
@@ -16,7 +16,6 @@ class AlerteType extends AbstractType
             ->add('libelle')
             ->add('laDate', DateType::class, ['widget' => 'single_text'])
             ->add('description')
-            ->add('intervention')
         ;
     }
 
