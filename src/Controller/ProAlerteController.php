716528bfe92acbdd67f834286ece41caf08c6270
Kévin Oliva <olivakevin64@gmail.com>
2020-03-17 14:08:04 +0100

    Professionnel : vision (index) des interventions et des factures du bien

--- src/Controller/ProAlerteController.php
+++ src/Controller/ProAlerteController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Alerte;
+use App\Form\AlerteType;
+use App\Repository\AlerteRepository;
+use App\Repository\AutorisationRepository;
+use App\Entity\Bien;
+use App\Repository\BienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProAlerteController extends AbstractController
 {
     /**
-     * @Route("/pro/alerte", name="pro_alerte_index")
+     * @Route("/pro/{idBien}/alerte", name="pro_alerte_index")
      */
-    public function index()
+    public function index(AlerteRepository $alerteRepository, AutorisationRepository $autorisationRepository, $idBien): Response
     {
-        return $this->render('pro_alerte/index.html.twig', [
-            'controller_name' => 'ProAlerteController',
-        ]);
+      // Récupérer le repository de l'entité Bien
+      $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
+      // Récupérer les biens enregistrés en BD
+      $bien = $repositoryBien->find($idBien);
+
+      return $this->render('professionnel/alerte/index.html.twig', [
+        'alertes' => $alerteRepository->findByIdBien($idBien),
+        'autorisations' => $autorisationRepository->findByIdBien($idBien),
+        'bien' => $bien
+      ]);
+    }
+
+    /**
+    * @Route("/{id}", name="alerte_show", methods={"GET"})
+    */
+    public function show(Alerte $alerte, $idBien): Response
+    {
+      // Récupérer le repository de l'entité Bien
+      $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
+      // Récupérer les biens enregistrés en BD
+      $bien = $repositoryBien->find($idBien);
+
+      return $this->render('alerte/show.html.twig', [
+        'alerte' => $alerte,
+        'bien' => $bien
+      ]);
     }
 }
--- src/Controller/ProInterventionController.php
+++ src/Controller/ProInterventionController.php
@@ -5,90 +5,100 @@ namespace App\Controller;
 use App\Entity\Intervention;
 use App\Form\InterventionType;
 use App\Repository\InterventionRepository;
+use App\Repository\AutorisationRepository;
+use App\Entity\Bien;
+use App\Repository\BienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/pro/intervention")
- */
+* @Route("/pro/{idBien}/intervention")
+*/
 class ProInterventionController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_intervention_index", methods={"GET"})
-     */
-    public function index(InterventionRepository $interventionRepository): Response
-    {
-        return $this->render('professionnel/intervention/index.html.twig', [
-            'interventions' => $interventionRepository->findAll(),
-        ]);
+  /**
+  * @Route("/", name="pro_intervention_index", methods={"GET"})
+  */
+  public function index(InterventionRepository $interventionRepository, AutorisationRepository $autorisationRepository, $idBien): Response
+  {
+    // Récupérer le repository de l'entité Bien
+    $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
+    // Récupérer les biens enregistrés en BD
+    $bien = $repositoryBien->find($idBien);
+
+    return $this->render('professionnel/intervention/index.html.twig', [
+      'interventions' => $interventionRepository->findAll(),
+      'autorisations' => $autorisationRepository->findByIdBien($idBien),
+      'bien' => $bien
+    ]);
+  }
+
+  /**
+  * @Route("/new", name="pro_intervention_new", methods={"GET","POST"})
+  */
+  public function new(Request $request): Response
+  {
+    $intervention = new Intervention();
+    $form = $this->createForm(InterventionType::class, $intervention);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($intervention);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('intervention_index');
     }
 
-    /**
-     * @Route("/new", name="pro_intervention_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $intervention = new Intervention();
-        $form = $this->createForm(InterventionType::class, $intervention);
-        $form->handleRequest($request);
+    return $this->render('professionnel/intervention/new.html.twig', [
+      'intervention' => $intervention,
+      'form' => $form->createView(),
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($intervention);
-            $entityManager->flush();
+  /**
+  * @Route("/{id}", name="pro_intervention_show", methods={"GET"})
+  */
+  public function show(Intervention $intervention): Response
+  {
+    return $this->render('professionnel/intervention/show.html.twig', [
+      'intervention' => $intervention,
+    ]);
+  }
 
-            return $this->redirectToRoute('intervention_index');
-        }
+  /**
+  * @Route("/{id}/edit", name="pro_intervention_edit", methods={"GET","POST"})
+  */
+  public function edit(Request $request, Intervention $intervention): Response
+  {
+    $form = $this->createForm(InterventionType::class, $intervention);
+    $form->handleRequest($request);
 
-        return $this->render('professionnel/intervention/new.html.twig', [
-            'intervention' => $intervention,
-            'form' => $form->createView(),
-        ]);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      return $this->redirectToRoute('intervention_index');
     }
 
-    /**
-     * @Route("/{id}", name="pro_intervention_show", methods={"GET"})
-     */
-    public function show(Intervention $intervention): Response
-    {
-        return $this->render('professionnel/intervention/show.html.twig', [
-            'intervention' => $intervention,
-        ]);
+    return $this->render('professionnel/intervention/edit.html.twig', [
+      'intervention' => $intervention,
+      'form' => $form->createView(),
+    ]);
+  }
+
+  /**
+  * @Route("/{id}", name="pro_intervention_delete", methods={"DELETE"})
+  */
+  public function delete(Request $request, Intervention $intervention): Response
+  {
+    if ($this->isCsrfTokenValid('delete'.$intervention->getId(), $request->request->get('_token'))) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($intervention);
+      $entityManager->flush();
     }
 
-    /**
-     * @Route("/{id}/edit", name="pro_intervention_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Intervention $intervention): Response
-    {
-        $form = $this->createForm(InterventionType::class, $intervention);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('intervention_index');
-        }
-
-        return $this->render('professionnel/intervention/edit.html.twig', [
-            'intervention' => $intervention,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="pro_intervention_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Intervention $intervention): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$intervention->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($intervention);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('pro_intervention_index');
-    }
+    return $this->redirectToRoute('pro_intervention_index');
+  }
 }
