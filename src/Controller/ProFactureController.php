bb6aa79673a674b85a8474190b96565708af2a08
Kévin Oliva <olivakevin64@gmail.com>
2020-03-22 15:33:58 +0100

    Ajout 'show' pour voir une facture - cé professionnel

--- src/Controller/ProFactureController.php
+++ src/Controller/ProFactureController.php
@@ -14,36 +14,43 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/pro/{idBien}/facture")
- */
+* @Route("/pro/{idBien}/facture")
+*/
 class ProFactureController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_facture_index", methods={"GET"})
-     */
-    public function index(FactureRepository $factureRepository, AutorisationRepository $autorisationRepository, $idBien): Response
-    {
-      // Récupérer le repository de l'entité Bien
-      $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
-      // Récupérer les biens enregistrés en BD
-      $bien = $repositoryBien->find($idBien);
+  /**
+  * @Route("/", name="pro_facture_index", methods={"GET"})
+  */
+  public function index(FactureRepository $factureRepository, AutorisationRepository $autorisationRepository, $idBien): Response
+  {
+    // Récupérer le repository de l'entité Bien
+    $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
+    // Récupérer les biens enregistrés en BD
+    $bien = $repositoryBien->find($idBien);
 
-        return $this->render('/professionnel/facture/index.html.twig', [
-            'factures' => $factureRepository->findAll(),
-            'autorisations' => $autorisationRepository->findByIdBien($idBien),
-            'bien' => $bien
-        ]);
-    }
+    return $this->render('/professionnel/facture/index.html.twig', [
+      'factures' => $factureRepository->findAll(),
+      'autorisations' => $autorisationRepository->findByIdBien($idBien),
+      'bien' => $bien
+    ]);
+  }
 
-    /**
-     * @Route("/{id}", name="pro_facture_show", methods={"GET"})
-     */
-    public function show(Facture $facture): Response
-    {
-        return $this->render('professionnel/facture/show.html.twig', [
-            'facture' => $facture,
-        ]);
-    }
+  /**
+  * @Route("/{id}", name="pro_facture_show", methods={"GET"})
+  */
+  public function show(Facture $facture, AutorisationRepository $autorisationRepository, $idBien): Response
+  {
+    // Récupérer le repository de l'entité Bien
+    $repositoryBien = $this->getDoctrine()->getRepository(Bien::class);
+    // Récupérer les biens enregistrés en BD
+    $bien = $repositoryBien->find($idBien);
+
+    return $this->render('professionnel/facture/show.html.twig', [
+      'facture' => $facture,
+      'autorisations' => $autorisationRepository->findByIdBien($idBien),
+      'bien' => $bien
+    ]);
+  }
 
 
 }
