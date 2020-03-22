f8fcf33e1a152992fb9ab522d2a5582c8f563402
Kévin Oliva <olivakevin64@gmail.com>
2020-03-22 19:24:29 +0100

    Amélioration du formulaire d'ajout d'un bien

--- src/Form/BienType.php
+++ src/Form/BienType.php
@@ -6,24 +6,28 @@ use App\Entity\Bien;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class BienType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('adresse')
-            ->add('ville')
-            ->add('codePostal')
-            ->add('dateConstruct')
-            ->add('surface')
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+    ->add('adresse')
+    ->add('ville')
+    ->add('codePostal')
+    ->add('dateConstruct', DateType::class, ['widget' => 'single_text',
+    'label' => 'Date de construction'])
+    ->add('surface', TextType::class, ['label' => 'Surface (en m2)'])
+    ;
+  }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Bien::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults([
+      'data_class' => Bien::class,
+    ]);
+  }
 }
